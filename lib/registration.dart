5bf5abd41b2429c3fa90afa8ca48589be2a1c13a
torayanushi <torayanushi@gmail.com>
2024-04-29 13:43:42 +0900

    added firebase authentication function

--- lib/registration.dart
+++ lib/registration.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/main.dart';
 
@@ -32,7 +33,90 @@ class Registration extends StatelessWidget {
             isPassword: true,
           ),
           ElevatedButton(
-              onPressed: () {},
+              onPressed: () async {
+                if (passwordCheck != password) {
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('エラー'),
+                          content: const Text('パスワードが一致していません'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('OK'),
+                            )
+                          ],
+                        );
+                      });
+                } else {
+                  if (mailAdress != null && password != null) {
+                    try {
+                      UserCredential userCredential = await FirebaseAuth
+                          .instance
+                          .createUserWithEmailAndPassword(
+                              email: mailAdress!, password: password!);
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('登録しました'),
+                              content: const Text('登録が完了しました。'),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.popUntil(
+                                          context, (route) => route.isFirst);
+                                    },
+                                    child: const Text('OK'))
+                              ],
+                            );
+                          });
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'weak-password') {
+                        // ここにアラートを表示させる処理を追加する
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text('エラー'),
+                                content: const Text('パスワードが簡単すぎます。'),
+                                actions: [
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text('OK'))
+                                ],
+                              );
+                            });
+                      } else if (e.code == 'email-already-in-use') {
+                        // ここにアラートを表示させる処理を追加する
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text('エラー'),
+                                content: const Text('入力されたメールアドレスは既に使われています。'),
+                                actions: [
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text('OK'))
+                                ],
+                              );
+                            });
+                      }
+                    } catch (e) {
+                      // ここにアラートを表示させる処理を追加する
+                      debugPrint(e.toString());
+                    }
+                  } else {}
+                }
+              },
               child: Container(
                 width: 200,
                 height: 50,
